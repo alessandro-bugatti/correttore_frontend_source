2f99a359115f3d4577f85c3b2d33dad5bf7d6b01
Alekos Filini <alekos.filini@gmail.com>
2016-08-16 21:33:03 +0200

    First commit, empty project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontendStableApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the frontendStableApp
+ */
+angular.module('frontendStableApp')
+    .controller('MainCtrl', function () {
+        this.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
