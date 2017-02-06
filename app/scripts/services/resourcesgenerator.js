3901bed430cc25dbfe561c2d4946cc14bd8c7ec9
Alekos Filini <alekos.filini@gmail.com>
2017-02-06 20:28:52 +0100

    Completata mappa degli errori

--- app/scripts/services/resourcesgenerator.js
+++ app/scripts/services/resourcesgenerator.js
@@ -11,31 +11,56 @@ angular.module('frontendStableApp')
     .service('ResourcesGeneratorService', function (Config, $resource, $q, $mdDialog, $rootScope) {
         var errors = {
             data: {
+                /* Errori generici */
                 'Not a valid request': 'Richiesta incompleta',
                 'User not found': 'Credenziali errate o inesistenti',
                 'Forbidden': 'Accesso negato',
                 'Unauthorized': 'Sessione scaduta o permessi insufficienti',
+                'Method not allowed by your role': 'Permessi insufficienti',
+                'Wrong role': 'Il tuo ruolo non permette quest\'azione',
+
+                /* Categorie */
                 'category already exist': 'Esiste già una categoria con questo nominativo',
+
+                /* Gruppi */
                 'group already exist': 'Esiste già un gruppo con questo nominativo',
                 'permission denied, user does not own this group': 'Il gruppo appartiene ad un altro utente',
                 'group does not exist or description is duplicated': 'Gruppo da modificare non valido',
                 'permission denied, user does not own this group or the group does not exist': 'Gruppo inesistente',
                 'permission denied, user is not a student or the student does not exist': 'L\'utente selezionato non esiste',
-                'Method not allowed by your role': 'Permessi insufficienti',
+
+                /* Problemi */
                 'You don\'t own the task': 'Problema inesistente',
+                'permission denied, task is public or the task does not exist': 'Il problema è pubblico o inesistente',
+                'permission denied, the task does not exist': 'Problema inesistente',
+                'The user doesn\'t own the task': 'Problema inesistente',
+                'The task doesn\'t exist': 'Problema inesistente',
+
+
+                /* Verifiche */
                 'permission denied, user does not own this test': 'Test inesistente',
                 'permission denied, user id is not correct': 'Studente inesistente',
                 'test already exist': 'Esiste già un test con questo nominativo',
                 'test does not exist or description is duplicated': 'Test da modificare non valido',
                 'permission denied, user does not own this test or the test does not exist': 'Test inesistente',
-                'permission denied, task is public or the task does not exist': 'Il problema è pubblico o inesistente',
-                'permission denied, the task does not exist': 'Problema inesistente'
+
+                /* Sottomissione */
+                'Task does not exist': 'Problema inesistente',
+                'Task is not public': 'Il problema è privato',
+                'Task is public': 'Il problema è pubblico',
+                'There is not the submitted file': 'Codice sorgente mancante',
+                'Only students can submit solutions': 'Solo gli studenti possono sottomettere una soluzione',
+                'This task is not in the current test': 'Il problema non appartiene ad una verifica',
+                'The test is no longer available': 'La verifica è conclusa',
+                'Only teachers can submit solutions': 'Solo gli insegnati possono ottenere i risultati',
+                'Solution not found': 'Soluzione non trovata'
             },
             code: {
                 409: 'Conflitto nella creazione della risorsa',
                 404: 'Risorsa non trovata',
                 403: 'Accesso negato',
-                401: 'Permessi insufficienti per accedere alla risorsa'
+                401: 'Permessi insufficienti per accedere alla risorsa',
+                400: 'Parametri mancanti'
             }
         };
 
