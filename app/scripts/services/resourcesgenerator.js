a9f16dc4af61f49eb2a5cfcd1c7855bebc8f428e
Alekos Filini <alekos.filini@gmail.com>
2017-02-06 20:17:49 +0100

    Aggiunta mappa degli errori

--- app/scripts/services/resourcesgenerator.js
+++ app/scripts/services/resourcesgenerator.js
@@ -9,6 +9,36 @@
  */
 angular.module('frontendStableApp')
     .service('ResourcesGeneratorService', function (Config, $resource, $q, $mdDialog, $rootScope) {
+        var errors = {
+            data: {
+                'Not a valid request': 'Richiesta incompleta',
+                'User not found': 'Credenziali errate o inesistenti',
+                'Forbidden': 'Accesso negato',
+                'Unauthorized': 'Sessione scaduta o permessi insufficienti',
+                'category already exist': 'Esiste già una categoria con questo nominativo',
+                'group already exist': 'Esiste già un gruppo con questo nominativo',
+                'permission denied, user does not own this group': 'Il gruppo appartiene ad un altro utente',
+                'group does not exist or description is duplicated': 'Gruppo da modificare non valido',
+                'permission denied, user does not own this group or the group does not exist': 'Gruppo inesistente',
+                'permission denied, user is not a student or the student does not exist': 'L\'utente selezionato non esiste',
+                'Method not allowed by your role': 'Permessi insufficienti',
+                'You don\'t own the task': 'Problema inesistente',
+                'permission denied, user does not own this test': 'Test inesistente',
+                'permission denied, user id is not correct': 'Studente inesistente',
+                'test already exist': 'Esiste già un test con questo nominativo',
+                'test does not exist or description is duplicated': 'Test da modificare non valido',
+                'permission denied, user does not own this test or the test does not exist': 'Test inesistente',
+                'permission denied, task is public or the task does not exist': 'Il problema è pubblico o inesistente',
+                'permission denied, the task does not exist': 'Problema inesistente'
+            },
+            code: {
+                409: 'Conflitto nella creazione della risorsa',
+                404: 'Risorsa non trovata',
+                403: 'Accesso negato',
+                401: 'Permessi insufficienti per accedere alla risorsa'
+            }
+        };
+
         this.getResource = function (authToken, path) {
             var headersObj = {};
             headersObj[Config.getAuthTokenName()] = authToken;
@@ -43,15 +73,16 @@ angular.module('frontendStableApp')
 
             var errorString = null;
             if (error && error.data && error.data.error != undefined)
-                errorString = error.data.error;
+                errorString = errors.data[error.data.error];
 
-            console.log(errorString);
+            if (!errorString)
+                errorString = errors.code[error.status];
 
             $mdDialog.show(
                 $mdDialog.alert()
                     .clickOutsideToClose(true)
                     .title('Si è verificato un errore')
-                    .textContent('')
+                    .textContent(errorString)
                     .ariaLabel('Messaggio di errore')
                     .ok('Chiudi')
                     .targetEvent($rootScope.getClickEvent())
